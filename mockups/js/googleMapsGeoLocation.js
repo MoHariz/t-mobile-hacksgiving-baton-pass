a8e54e32b33ce732455eefeeb87ea9dddada25ba
Jesse <jes4u@uw.edu>
2018-11-17 23:34:46 -0800

    Further looking into googlemaps

--- mockups/js/googleMapsGeoLocation.js
+++ mockups/js/googleMapsGeoLocation.js
@@ -4,7 +4,10 @@
 // failed.", it means you probably did not give permission for the browser to
 // locate you.
 var map, infoWindow;
+let location1;
+let location2;
 function initMap() {
+
 
 // Map initialization   ------------------------------------------------------------------------
   map = new google.maps.Map(document.getElementById('map'), {
@@ -25,6 +28,8 @@ function initMap() {
       infoWindow.setContent('Location found.');
       infoWindow.open(map);
       map.setCenter(pos);
+      location1 = pos;
+      console.log(location1)
     }, function() {
       handleLocationError(true, infoWindow, map.getCenter());
     });
@@ -32,6 +37,23 @@ function initMap() {
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
   }
+
+
+  // Event Listener for click to place marker -----------------------------------------------
+ 
+  var marker = new google.maps.Marker({
+    
+    map: map,
+    title: 'click to add marker'
+  });
+
+  map.addListener('click', function(e) {
+    marker.setPosition(e.latLng);
+    location2 = e.latLng;
+    console.log(e.latLng)
+  })
+
+
 }
 
 // Error Handling for geolocation ------------------------------------------------------------------------
@@ -42,5 +64,42 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
                         'Error: Your browser doesn\'t support geolocation.');
   infoWindow.open(map);
 }
+
+// Get Distance between two points WARNING MAKE SURE NO INFINITE LOOPS --------------------------------------
+window.onload = function() {
+
+  console.log("Loaded")
+  console.log("position 1" + location1)
+  document.getElementById("getDistance").onclick = getDist;
+
+
+function getDist() {
+  console.log("Uncomment code, solve async prob")
+  // var service = new google.maps.DistanceMatrixService();
+
+
+
+  // service.getDistanceMatrix(
+  // {
+  //   origins: [map.getCurrentPosition],
+  //   destinations: [location2],
+  //   travelMode: 'DRIVING',
+  //   avoidHighways: Boolean,
+  //   avoidTolls: Boolean,
+  // }, callback);
+
+  // }
+
+  // function callback(response, status) {
+  //   console.log(response);
+  // }
+
+}
+
+
+
+
+
+
 
 
